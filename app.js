d8c1e4205a9c6fd839d1d9aec174dbb0556ea771
Jake Shams <jshams62@gmail.com>
2018-12-11 08:13:55 -0800

    Users can see all reviews

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ const express = require('express')
 const app = express()
 var exphbs = require('express-handlebars');
 
+let reviews = [
+  { title: "Great Review", movieTitle: "Batman II" },
+  { title: "Awesome Movie", movieTitle: "Titanic" }
+]
+
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars')
 
+// INDEX
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('reviews-index', { reviews: reviews });
 })
 
 app.listen(3000, () => {
