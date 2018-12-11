9442a35667aa476bac5862063f34e8266941d0e5
Jake Shams <jshams62@gmail.com>
2018-12-11 08:37:21 -0800

    Users can now see single reviews

--- app.js
+++ app.js
@@ -31,11 +31,21 @@ app.get('/', (req, res) => {
 app.post('/reviews', (req, res) => {
   Review.create(req.body).then((review) => {
     console.log(review);
-    res.redirect('/');
+    res.redirect(`/reviews/${review._id}`) // Redirect to reviews/:id
   }).catch((err) => {
     console.log(err.message);
   })
 })
+
+// SHOW
+app.get('/reviews/:id', (req, res) => {
+  Review.findById(req.params.id).then((review) => {
+    res.render('reviews-show', { review: review })
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
+
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
